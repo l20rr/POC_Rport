49d5aaae66a42215ff79cd5c17cac67352aebc68
Lucas Rocha <l.rochasantos20@gmail.com>
2023-08-14 15:55:20 +0100

    Configuração geral projeto

---
+++ ReportApp.API/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using ReportApp.API.Models;
+using System;
+
+namespace ReportApp.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                using var context = scope.ServiceProvider.GetService<AppDbContext>();
+                context.Database.EnsureCreated();
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
