22e3328692006db09d2fe5559e30e40e4fdfba1e
Mariana Alberto <mariana_alberto@live.com.pt>
2023-08-17 10:28:04 +0100

    Added ReportApp.Shared

---
+++ ReportApp.Shared/Ranking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportApp.Shared
+{
+	public enum Ranking
+	{
+		Star1,
+		Star2,
+		Star3,
+		Star4,
+		Star5
+	}
+}
