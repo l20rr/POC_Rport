fb5d2ade19c2e7007070ba334ab44305cfdcba35
Lucas Rocha <l.rochasantos20@gmail.com>
2023-08-22 19:04:18 +0100

    email and description

--- ReportApp/Program.cs
+++ ReportApp/Program.cs
@@ -11,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddHttpClient<IBugReportDataService, BugReportDataService>(client => client.BaseAddress = new Uri("https://localhost:7046/"));
-
+builder.Services.AddHttpClient<IFeedbackDataService, FeedbackDataService>(client => client.BaseAddress = new Uri("https://localhost:7046/"));
+//builder.Services.AddHttpClient<IAttachmentDataService, AttachmentDataService>(client => client.BaseAddress = new Uri("https://localhost:7046/"));
 
 await builder.Build().RunAsync();
