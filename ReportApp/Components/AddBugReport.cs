c4c91d91b76b851203bc9268294f42a83d84ea47
Lucas Rocha <l.rochasantos20@gmail.com>
2023-08-24 13:17:40 +0100

    Ranking and style

--- ReportApp/Components/AddBugReport.cs
+++ ReportApp/Components/AddBugReport.cs
@@ -9,68 +9,97 @@ using System.Threading.Tasks;
 
 namespace ReportApp.Components
 {
-    public partial class AddBugReport
-    {
+	public partial class AddBugReport
+	{
 
 
-        private BugReport BugReport { get; set; } = new BugReport { UserId = 1, Timestamp = DateTime.Now, Description = "", AttachmentId = 1 };
+		private BugReport BugReport { get; set; } = new BugReport();
+		private User User { get; set; } = new User();
 
-        private User User { get; set; } = new User();
+		[Inject]
+		public IBugReportDataService BugReportDataService { get; set; }
 
-        [Inject]
-        public IBugReportDataService BugReportDataService { get; set; }
+		[Inject]
+		public IUserDataService UserDataService { get; set; }
+		public bool ShowReportForm { get; set; } = false;
 
-        public bool ShowReportForm { get; set; } = false;
+		[Parameter]
+		public EventCallback<bool> CloseEventCallback { get; set; }
 
-        [Parameter]
-        public EventCallback<bool> CloseEventCallback { get; set; }
+		private bool isInitialized = false;
+		private bool isShowing = false;
 
-        private bool isInitialized = false;
-        private bool isShowing = false;
+		protected override async Task OnInitializedAsync()
+		{
+			await base.OnInitializedAsync(); // Chame a implementação base primeiro
+			isInitialized = true;
+		}
 
-        protected override async Task OnInitializedAsync()
-        {
-            await base.OnInitializedAsync(); // Chame a implementação base primeiro
-            isInitialized = true;
-        }
 
-     
 
-        public async Task ShowAsync()
-        {
-            if (!isInitialized)
-            {
-                return;
-            }
+		public async Task ShowAsync()
+		{
+			if (!isInitialized)
+			{
+				return;
+			}
 
-            ResetReportForm();
-            isShowing = true;
-            StateHasChanged();
+			ResetReportForm();
+			isShowing = true;
+			StateHasChanged();
 
-            await Task.Delay(10); // Pequeno atraso para permitir que a renderização seja atualizada
+			await Task.Delay(10); // Pequeno atraso para permitir que a renderização seja atualizada
 
-            ShowReportForm = isShowing; // Atualize ShowReportForm após a renderização ser atualizada
-            StateHasChanged();
-        }
+			ShowReportForm = isShowing; // Atualize ShowReportForm após a renderização ser atualizada
+			StateHasChanged();
+		}
 
-        public void Close()
-        {
-            ShowReportForm = false;
-            StateHasChanged();
-        }
+		public void Close()
+		{
+			ShowReportForm = false;
+			StateHasChanged();
+		}
 
-        private void ResetReportForm()
-        {
-            BugReport = new BugReport { UserId = 1, Timestamp = DateTime.Now, Description = "", AttachmentId = 1 };
-        }
+		private void ResetReportForm()
+		{
+			BugReport = new BugReport { UserId = 1, Timestamp = DateTime.Now, Description = "", AttachmentId = 1 };
+		}
 
-        protected async Task HandleValidSubmit()
-        {
-            await BugReportDataService.AddBugReport(BugReport);
-            ShowReportForm = false;
+		private async Task AddBug(int userId)
+		{
 
-            await CloseEventCallback.InvokeAsync(true);
-            StateHasChanged();
-        }
-    }
+			if (!string.IsNullOrWhiteSpace(BugReport.Description))
+			{
+
+				BugReport.UserId = userId;
+
+
+				var response = await BugReportDataService.AddBugReport(BugReport);
+
+				if (response != null)
+				{
+					Console.WriteLine("Sucesso: ");
+				}
+				else
+				{
+					Console.WriteLine("Erro: Ocorreu um problema ao criar a carteira.");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Erro: O nome da carteira não pode ser vazio.");
+			}
+		}
+
+
+		private async Task HandleValidSubmit()
+		{
+			var response =  await UserDataService.AddUser(User);
+			await AddBug(response.UserId);
+			ShowReportForm = false;
+
+			await CloseEventCallback.InvokeAsync(true);
+			StateHasChanged();
+		}
+	}
 }
--- ReportApp/Components/AddFeedbackReport.cs
+++ ReportApp/Components/AddFeedbackReport.cs
@@ -15,9 +15,11 @@ namespace ReportApp.Components
 		public Feedback Feedback { get; set; } = new Feedback();
 		private User User { get; set; } = new User();
 
+
 		[Inject]
 		public IFeedbackDataService FeedbackDataService { get; set; }
-
+		[Inject]
+		public IUserDataService UserDataService { get; set; }
 
 		public bool ShowReportForm { get; set; }
 
@@ -25,7 +27,13 @@ namespace ReportApp.Components
 		[Parameter]
 		public EventCallback<bool> CloseEventCallback { get; set; }
 
+		private int selectedRating = -1;
 
+		private void UpdateRating(int rating)
+		{
+			selectedRating = rating;
+			Console.WriteLine($"Avaliação selecionada: {rating}");
+		}
 		public void Show()
 		{
 			ResetReportForm();
@@ -45,11 +53,36 @@ namespace ReportApp.Components
 		{
 			Feedback = new Feedback();
 		}
-
-
-		protected async Task HandleValidSubmit()
+		private async Task Addfeed(int userId)
 		{
-			await FeedbackDataService.AddFeedback(Feedback);
+
+			if (!string.IsNullOrWhiteSpace(Feedback.Comments))
+			{
+
+				Feedback.UserId = userId;
+
+
+				var response = await FeedbackDataService.AddFeedback(Feedback);
+
+				if (response != null)
+				{
+					Console.WriteLine("Sucesso: ");
+				}
+				else
+				{
+					Console.WriteLine("Erro: Ocorreu um problema ao criar a carteira.");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Erro: O nome da carteira não pode ser vazio.");
+			}
+		}
+
+		private async Task HandleValidSubmit()
+		{
+			var response = await UserDataService.AddUser(User);
+			await Addfeed(response.UserId);
 			ShowReportForm = false;
 
 			await CloseEventCallback.InvokeAsync(true);
